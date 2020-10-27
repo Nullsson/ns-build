50ea7426db200c67e4fdb18998f58bd071e24952
Oskar Mendel <brokenprogrammer@gmail.com>
2020-10-27 20:11:28 +0100

    Made use of config file to build the DynamicCode.

--- code/ns_build_main.cpp
+++ code/ns_build_main.cpp
@@ -24,8 +24,6 @@
 #include "ns_build_config.cpp"
 #include "ns_build_dynamic.cpp"
 
-// TODO(Oskar): Add helper functions to ns_build.h if needed.
-
 static int UseLogging = 0;
 
 void
@@ -41,13 +39,14 @@ Build(char *CodePath)
     ZeroMemory(&ProcessInformation, sizeof(ProcessInformation));
 
     // TODO(Oskar): Build compilation string dynamically and change theese values later on.
-    char BuildCommand[2048] = "clang-cl -D_CRT_SECURE_NO_DEPRECATE -nologo /Zi ";
-    //strcat(BuildCommand, CodePath);
+    char BuildCommand[2048] = "clang-cl -D_CRT_SECURE_NO_DEPRECATE -nologo /Zi";
     strcat(BuildCommand, " ");
     strcat(BuildCommand, CodePath);
-    strcat(BuildCommand, "/nsb.cpp "); // TODO(Oskar): This should later be read from the config.
+    strcat(BuildCommand, " ");
     strcat(BuildCommand, "/LD /link /out:nsb.dll");
 
+    printf("%s\n", BuildCommand);
+    
     // TODO(Oskar): Document arguments for self learning purposes.
     if (CreateProcess(NULL,
                       BuildCommand,
@@ -73,7 +72,7 @@ Build(char *CodePath)
 }
 
 // TODO(Oskar): Implement!
-void Run()
+void PerformBuildStep()
 {
     
 }
@@ -101,29 +100,30 @@ main(int argc, char **args)
         }
     }
 
-    // TODO(Oskar): Read config file
+    NSBuildConfig Config = {0};
     {
         char CurrentDirectory[MAX_PATH];
         DWORD PathSize = GetCurrentDirectory(MAX_PATH, CurrentDirectory);
 
         strcat(CurrentDirectory, "/buildconfig.nsbconf");
         
-        NSBuildConfig Config = NSBuildConfigLoad(CurrentDirectory);
+        Config = NSBuildConfigLoad(CurrentDirectory);
         printf("DynamicCodeFileName: %s\n", Config.DynamicCodeFileName);
         printf("EntryFileName: %s\n", Config.EntryFileName);
         printf("CompilerBackend: %s\n", Config.CompilerBackend);
         printf("CompilerArguments: %s\n", Config.CompilerArguments);
         printf("LinkerArguments: %s\n", Config.LinkerArguments);
-        NSBuildConfigUnload(&Config);
     }
 
     char DynamicCodePath[2048];
     DWORD Size = GetCurrentDirectory(2048, DynamicCodePath);
     if (Size)
     {
-        // TODO(Oskar): Name of this file should be gotten from the config.
-        printf("%s\n", DynamicCodePath);
-        Build(DynamicCodePath);
+        unsigned int FileNameSize = StringLength(Config.DynamicCodeFileName);
+        char Buffer[Size + FileNameSize + 1];
+        snprintf(Buffer, sizeof(Buffer), "%s\\%s", DynamicCodePath, Config.DynamicCodeFileName);
+
+        Build(Buffer);
     }
 
     // TODO(Oskar): Logging.
@@ -134,7 +134,9 @@ main(int argc, char **args)
         DynamicCode.InitCallback();
     }
     
-    Run();
+    PerformBuildStep();
+
+    NSBuildConfigUnload(&Config);
     
     return (0);
 }

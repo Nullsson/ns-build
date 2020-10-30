af6a5801efca2a8580e0a18089db12806329c02b
Oskar Mendel <brokenprogrammer@gmail.com>
2020-10-30 21:00:15 +0100

    Updated pre_post build example with build timing example code.

--- examples/pre_post_build_demo/nsb.cpp
+++ examples/pre_post_build_demo/nsb.cpp
@@ -8,16 +8,30 @@
 
 #include "../../code/ns_build.h"
 #include <stdio.h>
+#include <Windows.h>
+
+LARGE_INTEGER Frequency;
+LARGE_INTEGER StartingTime;
+LARGE_INTEGER EndingTime;
 
 NSBUILD_FUNC void
 NSBuildCustomInitCallback(void)
 {
     printf("This is a custom InitCallback function\n");
-    printf("This will be ran before the build.\n");
+    printf("Code here will be ran before the compile of your application.\n");
+
+    QueryPerformanceFrequency(&Frequency);
+    QueryPerformanceCounter(&StartingTime);
 }
 
 NSBUILD_FUNC void
 NSBuildCustomCleanUpCallback(void)
 {
+    printf("This is a custom CleanupCallback function\n");
+    printf("Code here will be ran after the compile of your application.\n");
+    QueryPerformanceCounter(&EndingTime);
+    double ElapsedTime = (double)(EndingTime.QuadPart - StartingTime.QuadPart) / (double)Frequency.QuadPart;
+    ElapsedTime *= 1000.0;
 
+    printf("Build took %fms", ElapsedTime);
 }
